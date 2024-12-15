d2b877e1a0d70d4f5c8c078ec71b9fd7289f8647
Camila Flores <camila_maria@unitec.edu>
2024-12-14 19:52:00 -0600

    Add files via upload

---
+++ Raiz/src/raiz/Raiz.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package raiz;
+
+import java.util.Scanner;
+
+public class Raiz {
+
+    public static void main(String[] args) {
+        
+        Scanner numero = new Scanner(System.in);
+        //importar scanner
+        //scanner muestra consola
+        double raiz;
+        int numero
+        
+        System.out.println("Favor ingresar un numero");
+        raiz = Math.sqrt(numero.nextDouble());
+        System.out.println("La raiz cuadrada es:" +raiz);
+        
+     
+            
+        }
+       
+        
+        
+           }
+    
+}
